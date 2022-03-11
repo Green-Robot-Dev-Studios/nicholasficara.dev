91e72444498bd914fa917c44221aa8a6be56bc97
Nicholas Ficara <nficara01@gmail.com>
2022-03-11 08:12:34 -0500

    Add bday card

---
+++ other/bday/terminal.mjs
@@ -0,0 +1,37 @@
+
+import { input, type } from "./util/io.js";
+import { toggleFullscreen } from "./util/screens.js";
+import { registerHandlers } from "./util/ui.mjs";
+
+async function onLoad() {
+	toggleFullscreen(true);
+	run();
+}
+
+async function run() {
+	const { power } = await import("./util/power.js");
+	const { parse } = await import("./util/io.js");
+
+	// Turns on the screen
+	power();
+
+	let command = "\n  _    _ _   _____                  _                                         \n | |  | (_) |  __ \\                | |                                        \n | |__| |_  | |__) |_ _ _ __   __ _| |                                        \n |  __  | | |  ___\/ _` | \'_ \\ \/ _` | |                                        \n | |  | | | | |  | (_| | |_) | (_| |_|                                        \n |_|  |_|_| |_|   \\__,_| .__\/ \\__,_(_)_  _      _   _         _             _ \n | |  | |              | |        |  _ \\(_)    | | | |       | |           | |\n | |__| | __ _ _ __  _ |_| _   _  | |_) |_ _ __| |_| |__   __| | __ _ _   _| |\n |  __  |\/ _` | \'_ \\| \'_ \\| | | | |  _ <| | \'__| __| \'_ \\ \/ _` |\/ _` | | | | |\n | |  | | (_| | |_) | |_) | |_| | | |_) | | |  | |_| | | | (_| | (_| | |_| |_|\n |_|  |_|\\__,_| .__\/| .__\/ \\__, | |____\/|_|_|   \\__|_| |_|\\__,_|\\__,_|\\__, (_)\n              | |   | |     __\/ |                                      __\/ |  \n              |_|   |_|    |___\/                                      |___\/   \n";
+	await type("> " + command);
+
+	while (true) {
+		await type("> Enter the secret code to gain access (hint: your age today): ")
+		let age = await input(true);
+		if (age == 49) {
+			break;
+		}
+		await type("> Input failed! Try again!")
+	}
+
+
+	await type("> Executing program…	")
+	await type("> Hello! You have unlocked your message. 	")
+	await type("> I am so grateful for all that you have done for me. Enjoy your special day!	")
+	await type("> ")
+}
+
+window.addEventListener("load", onLoad);
